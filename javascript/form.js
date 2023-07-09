ff737dd348c5a39cc417312dfc69f3db1536a703
harsh-si14 <harsh.vardhan1_cs20@gla.ac.in>
2023-07-09 16:12:20 +0530

    we

--- javascript/form.js
+++ javascript/form.js
@@ -31,32 +31,46 @@
 // }
   
 
-function submitForm(event) {
-  event.preventDefault(); // Prevent form submission
+function sendEmail() {
+  // event.preventDefault(); // Prevent form submission
 
   // Get form values
-  var name = document.getElementById("name").value;
-  var contactNumber = document.getElementById("contactNumber").value;
-  var email = document.getElementById("email").value;
-  var subject = document.getElementById("subject").value;
-  var message = document.getElementById("message").value;
+  // var name = document.getElementById("name").value;
+  // var contactNumber = document.getElementById("contactNumber").value;
+  // var email = document.getElementById("email").value;
+  // var subject = document.getElementById("subject").value;
+  // var message = document.getElementById("message").value;
 
-  // Perform form validation
-  if (!name || !contactNumber || !email || !subject || !message) {
-    alert("Please fill in all fields.");
-    return;
-  }
+  // // Perform form validation
+  // if (!name || !contactNumber || !email || !subject || !message) {
+  //   alert("Please fill in all fields.");
+  //   return;
+  // }
 
   // Construct URL parameters
-  var params = "?name=" + encodeURIComponent(name)
-    + "&contactNumber=" + encodeURIComponent(contactNumber)
-    + "&email=" + encodeURIComponent(email)
-    + "&subject=" + encodeURIComponent(subject)
-    + "&message=" + encodeURIComponent(message);
+  // var params = "?name=" + encodeURIComponent(name)
+  //   + "&contactNumber=" + encodeURIComponent(contactNumber)
+  //   + "&email=" + encodeURIComponent(email)
+  //   + "&subject=" + encodeURIComponent(subject)
+  //   + "&message=" + encodeURIComponent(message);
 
   // Display pop-up message
-  alert("Thank you! Your form has been submitted successfully.");
+  // alert("Thank you! Your form has been submitted successfully.");
 
   // Clear form fields
-  document.getElementById("contactForm").reset();
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "harshvardhan7022002@gmail.com",
+    Password : "Harsh#123",
+    To : 'singhrajkeshar@gmail.com',
+    From : document.getElementById("email").value,
+    Subject : document.getElementById("subject").value,
+    Body : "Name :" + document.getElementById("name").value + 
+    "<br> Email :" + document.getElementById("email").value + 
+    "<br> Subject :" + document.getElementById("subject").value + 
+    "<br> Massage :" + document.getElementById("massage").value
+  }).then(
+    message => alert("Thank you! Your form has been submitted successfully.")
+  );
+
 }
